5eb2753d0a556301f4fbaab096ade805bf3c8d0b
nika.zerekidze <nika.zerekidze@flatrocktech.com>
2024-06-03 15:53:51 +0400

    modified code like I wanted to, changed some variable names code placement added enter key event

--- PingPong.go
+++ PingPong.go
@@ -23,47 +23,62 @@ func emitStr(s tcell.Screen, x, y int, style tcell.Style, str string) {
 	}
 }
 
-func displayHelloWorld(s tcell.Screen) {
-	w, h := s.Size()
-	s.Clear()
+func displayWelcomeSceen(screen tcell.Screen) {
+	w, h := screen.Size()
+	screen.Clear()
 	welcomeText := "Welcome to my ping pong game"
-	text := (w - len(welcomeText)) / 2
-	style := tcell.StyleDefault.Foreground(tcell.Color30).Background(tcell.ColorWhite)
-	emitStr(s, text, h/2, style, "Welcome to my ping pong game")
-	emitStr(s, w/2-9, h/2+1, tcell.StyleDefault, "Press ESC to exit.")
-	s.Show()
+	exitText := "Press Enter to exit"
+	playSomeoneText := "Press Enter to play with someone"
+
+	welcomeTextPosition := (w - len(welcomeText)) / 2
+	playSomeoneTextPosition := (w - len(playSomeoneText)) / 2
+	exitTextPosition := (w - len(exitText)) / 2
+
+	style := tcell.StyleDefault.Foreground(tcell.Color30).Background(tcell.ColorBlack)
+	emitStr(screen, welcomeTextPosition, h/2, style, welcomeText)
+	emitStr(screen, exitTextPosition, h/2+1, tcell.StyleDefault, exitText)
+	emitStr(screen, playSomeoneTextPosition, h/2+2, tcell.StyleDefault, playSomeoneText)
+
+	screen.Show()
 }
 
-// This program just prints "Hello, World!".  Press ESC to exit.
 func main() {
 
-	s, e := tcell.NewScreen()
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", e)
+	screen := InintScreen()
+
+	displayWelcomeSceen(screen)
+
+	for {
+		switch ev := screen.PollEvent().(type) {
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape {
+				screen.Fini()
+				os.Exit(0)
+			}
+			if ev.Key() == tcell.KeyEnter {
+				screen.Beep()
+
+			}
+
+		}
+	}
+}
+
+func InintScreen() tcell.Screen {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-	if e := s.Init(); e != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", e)
+	if err := screen.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 
 	defStyle := tcell.StyleDefault.
 		Background(tcell.ColorBlack).
 		Foreground(tcell.ColorWhite)
-	s.SetStyle(defStyle)
+	screen.SetStyle(defStyle)
 
-	displayHelloWorld(s)
-
-	for {
-		switch ev := s.PollEvent().(type) {
-		case *tcell.EventResize:
-			s.Sync()
-			displayHelloWorld(s)
-		case *tcell.EventKey:
-			if ev.Key() == tcell.KeyEscape {
-				s.Fini()
-				os.Exit(0)
-			}
-		}
-	}
+	return screen
 }
