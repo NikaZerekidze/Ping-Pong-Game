52e5680add8b30c854feafc73ede8a2f34bfa448
nika.zerekidze <nika.zerekidze@flatrocktech.com>
2024-06-04 13:30:02 +0400

    Created paddle object made functions for initializing object and displaying them, modified code to be more clean

--- PingPong.go
+++ PingPong.go
@@ -7,9 +7,22 @@ import (
 	"os"
 )
 
-//// This is just starter code copied from tcell package demo folder. For rendering simple screen
+const (
+	PaddleSymbol = 0x2588
+	PaddleHeight = 4
+)
 
-func emitStr(s tcell.Screen, x, y int, style tcell.Style, str string) {
+type Paddle struct {
+	x, y, width, height int
+}
+
+var (
+	paddle1 *Paddle
+	paddle2 *Paddle
+)
+
+// / this is not written by me this comes with tcell library
+func emitStr(screen tcell.Screen, x, y int, style tcell.Style, str string) {
 	for _, c := range str {
 		var comb []rune
 		w := runewidth.RuneWidth(c)
@@ -18,11 +31,24 @@ func emitStr(s tcell.Screen, x, y int, style tcell.Style, str string) {
 			c = ' '
 			w = 1
 		}
-		s.SetContent(x, y, c, comb, style)
+		screen.SetContent(x, y, c, comb, style)
 		x += w
 	}
 }
 
+// / here paddles are paddle object is created
+func PrintGameObjectPaddles(screen tcell.Screen, x, y, width, height int, character rune) {
+
+	for col := 0; col < width; col++ {
+		for row := 0; row < height; row++ {
+			screen.SetContent(x+col, y+row, character, nil, tcell.StyleDefault)
+
+		}
+	}
+
+}
+
+// / this is main manu screen where welcome text is located
 func displayWelcomeSceen(screen tcell.Screen) {
 	w, h := screen.Size()
 	screen.Clear()
@@ -42,28 +68,56 @@ func displayWelcomeSceen(screen tcell.Screen) {
 	screen.Show()
 }
 
+// / this is function to display game objects (game screen) paddles, ball
+func displayGameScreen(screen tcell.Screen) {
+	screen.Clear()
+
+	PrintGameObjectPaddles(screen, paddle1.x, paddle1.y, paddle1.width, paddle1.height, PaddleSymbol)
+	PrintGameObjectPaddles(screen, paddle2.x, paddle2.y, paddle2.width, paddle2.height, PaddleSymbol)
+
+	screen.Show()
+}
+
 func main() {
 
 	screen := InintScreen()
 
 	displayWelcomeSceen(screen)
+	InitGameState(screen)
 
+	//PrintGameObjectPaddles(screen, 0, 0, 2, 5, '&')
+	isWelcomeScreen := true
 	for {
 		switch ev := screen.PollEvent().(type) {
+
 		case *tcell.EventKey:
 			if ev.Key() == tcell.KeyEscape {
 				screen.Fini()
 				os.Exit(0)
 			}
 			if ev.Key() == tcell.KeyEnter {
-				screen.Beep()
-
+				if isWelcomeScreen {
+					displayGameScreen(screen)
+					isWelcomeScreen = false
+				} else {
+					screen.Beep()
+					displayWelcomeSceen(screen)
+					isWelcomeScreen = true
+				}
 			}
-
+		case *tcell.EventResize:
+			if isWelcomeScreen {
+				displayWelcomeSceen(screen)
+			} else {
+				InitGameState(screen) /// when sceen size is changed I need to call screen again to update widht and height
+				displayGameScreen(screen)
+			}
 		}
+
 	}
 }
 
+// / this is for creating screen in general
 func InintScreen() tcell.Screen {
 	screen, err := tcell.NewScreen()
 	if err != nil {
@@ -81,4 +135,19 @@ func InintScreen() tcell.Screen {
 	screen.SetStyle(defStyle)
 
 	return screen
+}
+
+// / in here I am creating initializing game objects for displaying game object function
+func InitGameState(screen tcell.Screen) {
+	width, height := screen.Size()
+	paddleStart := height/2 - PaddleHeight/2
+
+	paddle1 = &Paddle{
+		x: 0, y: paddleStart, width: 1, height: PaddleHeight,
+	}
+
+	paddle2 = &Paddle{
+		x: width - 1, y: paddleStart, width: 1, height: PaddleHeight,
+	}
+
 }
